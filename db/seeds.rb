719d52d2707f7d677b2a7d4b8993c1d93815c526
cidermitaina <cidermitaina@gmail.com>
2019-07-10 14:40:39 +0900

    feat(note): Add note function

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times { Note.create!(body: 'Sample Task') }
+2.times { Note.create!(body: 'Sample Task') }
